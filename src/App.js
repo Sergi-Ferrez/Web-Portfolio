6911afc69e04c4ef8c13ef62746cf365cba60afc
Sergi <sergiferrezpuig@gmail.com>
2022-06-30 11:00:35 +0200

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import { Button } from 'bootstrap';
+import LanguagePicker from './components/LanguagePicker';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="App Text">
+      <header className="Header">
+        <LanguagePicker/>
+        <Button>asdas</Button>
       </header>
     </div>
   );
---
+++ src/components/LanguagePicker.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function LanguagePicker(){
+    return (
+      <div className="App">
+        <header className="Header">
+          <ul className="LanguagePicker">
+            <li>SP</li>
+            <li>EN</li>
+          </ul>
+        </header>
+      </div>
+    );
+}
+
+export default LanguagePicker;
